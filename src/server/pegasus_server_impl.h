232c66d02500a7583fc5901dccb9942c317c9308
heyuchen <heyuchen@xiaomi.com>
2020-04-16 17:29:17 +0800

    update rdsn and use is_bulk_load_ingestion flag instead of partition_version to reject write during ingestion

--- src/server/pegasus_server_impl.h
+++ src/server/pegasus_server_impl.h
@@ -152,8 +152,6 @@ public:
 
     virtual void query_app_envs(/*out*/ std::map<std::string, std::string> &envs) override;
 
-    virtual void set_partition_version(int32_t partition_version) override;
-
     virtual void set_ingestion_status(::dsn::replication::ingestion_status::type status) override;
 
     virtual ::dsn::replication::ingestion_status::type get_ingestion_status() const override
@@ -348,7 +346,6 @@ private:
 
     pegasus_manual_compact_service _manual_compact_svc;
 
-    std::atomic<int32_t> _partition_version;
     ::dsn::replication::ingestion_status::type _ingestion_status;
 
     dsn::task_tracker _tracker;
